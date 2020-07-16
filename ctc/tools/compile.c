c8635f6140e6e09bdb28a6213c8ca608c713a052
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2020-07-16 01:26:44 -0400

    compiler pain

--- ctc/tools/compile.c
+++ ctc/tools/compile.c
@@ -4,9 +4,99 @@
 
 static int posixNext(void* stream) { return fgetc(stream); }
 
-static void compileUnit(CtAST* unit)
+typedef enum {
+    CCT_I8086,
+    CCT_X86,
+    CCT_X64
+} CtCompileTarget;
+
+typedef struct HashEntry {
+    char* key;
+    int used;
+    void* data;
+
+    HashEntry* next;
+} HashEntry;
+
+typedef struct {
+    int size;
+    HashEntry* data;
+} HashMap;
+
+static HashMap hashNew(int size)
 {
-    (void)unit;
+    HashMap map;
+    map.size = size;
+    map.data = malloc(sizeof(HashEntry) * size);
+
+    for (int i = 0; i < size; i++)
+    {
+        map.data[i].used = 0;
+        map.data[i].next = NULL;
+    }
+
+    return map;
+}
+
+static void hashAdd(HashMap* self, char* key, void* data)
+{
+
+}
+
+static void hashGet(HashMap* self, char* key)
+{
+
+}
+
+typedef struct {
+    FILE* out;
+    CtCompileTarget target;
+} CtCompile;
+
+typedef struct {
+    int sign;
+    int width;
+} CtBuiltinType;
+
+typedef struct {
+    int len;
+    struct CtType* of;
+} CtArrayType;
+
+typedef struct CtType {
+    enum { CTT_BUILTIN, CTT_PTR, CTT_ARR } type;
+
+    union {
+        CtBuiltinType builtin;
+        struct CtType* ptr;
+        CtArrayType arr;
+    };
+} CtType;
+
+static CtType* resolveType(CtCompile* self, CtAST* type)
+{
+
+}
+
+static size_t typeSize(CtCompile* self, CtAST* type)
+{
+    
+}
+
+/**
+ * calculate required stack size for function
+ */
+static size_t stackSize(CtCompile* self, CtAST* func)
+{
+    size_t size = 0;
+}
+
+static void compileUnit(CtCompile* self, CtAST* unit)
+{
+    for (size_t i = 0; i < unit->data.unit.symbols.len; i++)
+    {
+
+    }
 }
 
 int main(int argc, const char** argv)
@@ -18,9 +108,13 @@ int main(int argc, const char** argv)
     CtLexer lex = ctLexOpen(&stream);
     CtParser parse = ctParseOpen(&lex);
 
+    CtCompile compile;
+    compile.target = CCT_I8086;
+    compile.out = fopen("out.asm", "wt");
+
     CtAST* unit = ctParseUnit(&parse);
 
-    compileUnit(unit);
+    compileUnit(&compile, unit);
 
     return 0;
 }
