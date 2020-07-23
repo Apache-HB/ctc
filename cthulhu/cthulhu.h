da1fa28aa81b462207f2e81a2e17c7833e1c8777
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2020-07-23 00:56:39 -0400

    i am going to kermit a homicide if i cant figure out how to do this shit
    AAAAAAAAAAAAAAAAAAAA

--- cthulhu/cthulhu.h
+++ cthulhu/cthulhu.h
@@ -4,16 +4,32 @@
 #include <stdint.h>
 #include <stddef.h>
 
-typedef size_t CtSize;
 
-typedef int(*CtNextFunc)(void*);
-typedef void*(CtAlloc)(void*, CtSize);
-typedef void(CtDealloc)(void*, void*);
 
-typedef struct {
-    void *data;
-    CtAlloc alloc;
-    CtDealloc dealloc;
-} CtState;
+typedef struct CtAST {
+    enum { 
+        UNARY,
+        BINARY,
+        TERNARY,
+        LITERAL
+    } type;
+
+    union {
+        struct CtAST *unary;
+
+        struct {
+            struct CtAST *lhs;
+            struct CtAST *rhs;
+        } binary;
+
+        struct {
+            struct CtAST *cond;
+            struct CtAST *yes;
+            struct CtAST *no;
+        } ternary;
+
+
+    };
+} CtAST;
 
 #endif /* CTHULHU_H */
