e95390e97e53f5929b3e2d61c2d10d2b5c40d862
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2020-10-23 14:29:06 -0400

    error erporting

--- cthulhu/cthulhu.cpp
+++ cthulhu/cthulhu.cpp
@@ -1,1 +1,20 @@
 #include "cthulhu.h"
+
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    (void)argc;
+    void* file = (void*)fopen(argv[1], "r");
+    auto stream = Stream(file, [](void* f) -> int {
+        auto n = fgetc((FILE*)f);
+        if (n == -1)
+            return 0;
+        return n;
+    }, argv[1]);
+
+    while (stream.next() != 0);
+
+    Where here = { 9, 1, 9, 11, &stream };
+
+    fmt::print("{}", underline(here, "something\nvery\nlong", { "funny", "thing" }));
+}
--- cthulhu/cthulhu.h
+++ cthulhu/cthulhu.h
@@ -2,25 +2,39 @@
 
 #include <variant>
 #include <string>
+#include <fmt/core.h>
 
 struct Where {
     int dist = 0;
     int line = 0;
     int col = 0;
     int len = 0;
+    const struct Stream* source;
 };
 
 struct Stream {
-    Stream(void* in, int(*func)(void*))
+    Stream(void* in, int(*func)(void*), std::string id)
         : stream(in)
         , get(func)
+        , name(id)
     { 
         ahead = get(stream);
+        pos.source = this;
     }
 
     int next() { 
         int c = ahead;
         ahead = get(stream);
+
+        buffer.push_back(c);
+
+        pos.dist++;
+        if (c == '\n') {
+            pos.line++;
+            pos.col = 0;
+        } else {
+            pos.col++;
+        }
 
         return c;
     }
@@ -32,22 +46,64 @@ struct Stream {
     void* stream;
     int(*get)(void*);
     int ahead;
+    std::string name;
     Where pos;
+
+    std::string buffer;
 };
 
-enum struct Key {
+std::vector<std::string> split(std::string str, std::string tok) {
+    std::vector<std::string> out;
+    while (str.size()) {
+        auto idx = str.find(tok);
+        if (idx != std::string::npos) {
+            out.push_back(str.substr(0, idx));
+            str = str.substr(idx + tok.size());
+            if (!str.size())
+                out.push_back(str);
+        } else {
+            out.push_back(str);
+            str = "";
+        }
+    }
+    return out;
+}
 
-};
+std::string underline(
+    Where it, 
+    const std::string& msg = "",
+    const std::vector<std::string>& notes = {}
+) {
+    auto in = it.source;
+    std::string out = fmt::format("{} -> [{}:{}]\n", in->name, it.line, it.col);
 
-struct Token {
-    std::variant<Key> data;
-    Where where;
-};
+    auto len = fmt::format(" {} ", it.line).length();
 
-struct Lexer {
-    Token next() {
+    auto begin = std::max(it.dist - it.col, 0);
+    auto end = in->buffer.find_first_of("\n\0", it.dist);
 
+    auto line = in->buffer.substr(begin, end);
+
+    out += std::string(len, ' ') + "|\n";
+    out += fmt::format(" {} | ", it.line) + line + "\n";
+    auto under = std::string(len, ' ') + "| " + std::string(it.col, ' ') + '^' + std::string(it.len - 1, '~');
+    out += under;
+
+    if (msg.length() > 0) {
+        bool once = true;
+        for (auto part : split(msg, "\n")) {
+            if (once) {
+                once = false;
+                out += " " + part + '\n';
+            } else {
+                out += std::string(under.length() + 1, ' ') + part + '\n';
+            }
+        }
     }
 
-    Stream in;
-};
+    for (auto note : notes) {
+        out += std::string(len, ' ') + "* " + note + '\n';
+    }
+
+    return out;
+}
