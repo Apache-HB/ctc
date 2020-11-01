559063f7e5b50e24cf876d9c9f2d6d1333db155b
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2020-11-01 00:23:01 -0400

    fix compilation errors

--- com/cthulhu/tokens/InvalidToken.java
+++ com/cthulhu/tokens/InvalidToken.java
@@ -3,7 +3,16 @@ package com.cthulhu.tokens;
 import com.cthulhu.Token;
 
 public class InvalidToken extends Token<String> {
+    public InvalidToken() {
+        super(null);
+    }
+
     public InvalidToken(String msg) {
         super(msg);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return false;
     }
 }
